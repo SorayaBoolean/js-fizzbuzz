9a33f5b5b74fd59b58900475e4e31f1da9f2059e
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-06 16:31:36 +0200

    css add: added h1 title and its rules as light text margin top and text align center above boxes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//creo ciclo di box * 100
 const containerDom = document.getElementById ('container')
 
 for (let i = 0; i <= 100; i ++) {
     containerDom.innerHTML += `<div class="box">${i}</div>`;
 }
+//
