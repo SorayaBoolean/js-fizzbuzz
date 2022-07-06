fffbaaf14d2bf1c6319aa62453116a9a86302dc2
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-06 17:21:16 +0200

    trying to give a class from js to html in 'box' that is alreasy a class

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ const containerDom = document.getElementById ('container')
 for (let i = 0; i <= 100; i ++) {
     containerDom.innerHTML += `<div class="box">${i}</div>`;
 }
-//
+//metto per ogni moltiplicatore di 3 colore verde e parola fizz
+
+//dare una classe a box
+let boxDom = document.querySelector ('box');
+boxDom.className += 'background' ;
+
+if  (3 % 3 == 0) {
+    document.getDocument
+}
