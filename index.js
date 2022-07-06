622c9ed2009b3bd994b0b6ca88c7cb52b1648e1a
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-06 18:10:00 +0200

    add

--- index.js
+++ index.js
@@ -2,14 +2,6 @@
 const containerDom = document.getElementById ('container')
 
 for (let i = 0; i <= 100; i ++) {
-    containerDom.innerHTML += `<div class="box">${i}</div>`;
+    containerDom.innerHTML += `<div id="box">${i}</div>`;
 }
-//metto per ogni moltiplicatore di 3 colore verde e parola fizz
 
-//dare una classe a box
-let boxDom = document.querySelector ('box');
-boxDom.className += 'background' ;
-
-if  (3 % 3 == 0) {
-    document.getDocument
-}
